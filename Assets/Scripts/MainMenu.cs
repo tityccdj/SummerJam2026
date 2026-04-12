3a06edfbe51cf724036cffc67d9fecc70e5dce4b
Bhumintra Chantanasevi <bhumintra.ch@gmail.com>
2026-04-12 18:47:19 +0700

    add setting page

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,14 +4,36 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField]
     private UIMainMenu uiMainMenu;
+    [SerializeField]
+    private UISetting uiSetting;
 
     void Start()
     {
         uiMainMenu.Setup(new UIMainMenu.Param
         {
             onPlay = () => SceneLoader.Instance.LoadScene("Game"),
-            onSetting = () => Debug.Log("Setting"),
+            onSetting = () =>
+            {
+                uiMainMenu.gameObject.SetActive(false);
+                uiSetting.gameObject.SetActive(true);
+            },
             onExit = () => Application.Quit(),
         });
+        uiSetting.Setup(new UISetting.Param
+        {
+            mainVolume = AudioManager.Instance.GetMasterVolume(),
+            bgmVolume = AudioManager.Instance.GetMusicVolume(),
+            sfxVolume = AudioManager.Instance.GetSFXVolume(),
+            onMainVolumeChanged = value => AudioManager.Instance.SetMasterVolume(value),
+            onBgmVolumeChanged = value => AudioManager.Instance.SetMusicVolume(value),
+            onSfxVolumeChanged = value => AudioManager.Instance.SetSFXVolume(value),
+            onBack = () =>
+            {
+                uiMainMenu.gameObject.SetActive(true);
+                uiSetting.gameObject.SetActive(false);
+            },
+        });
+        uiMainMenu.gameObject.SetActive(true);
+        uiSetting.gameObject.SetActive(false);
     }
 }
